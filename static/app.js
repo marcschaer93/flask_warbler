f8cb012fe5675b5581c238e61ecc79474872d68a
Marc Schär <admin@admins-MacBook-Pro.local>
2023-07-31 07:03:09 +0200

    render_deploy

--- static/app.js
+++ static/app.js
@@ -1,21 +1,21 @@
 
 
-async function toggleFavoriteStatus(evt) {
-    console.debug("toggleFavoriteStatus");
+// async function toggleFavoriteStatus(evt) {
+//     console.debug("toggleFavoriteStatus");
   
-    const $closestLi = $(evt.target).closest("li");
-    const storyId = $closestLi.attr("id");
-    const story = storyList.stories.find((s) => s.storyId === storyId);
-    const $target = $(evt.target);
+//     const $closestLi = $(evt.target).closest("li");
+//     const storyId = $closestLi.attr("id");
+//     const story = storyList.stories.find((s) => s.storyId === storyId);
+//     const $target = $(evt.target);
   
-    if ($target.hasClass("fa-solid")) {
-      $target.closest("i").toggleClass("fa-solid fa-regular");
-      await currentUser.deleteFavorite(story);
-      putMyFavoritesStoriesOnPage();
-    } else {
-      $target.closest("i").toggleClass("fa-solid fa-regular");
-      await currentUser.addFavorite(story);
-    }
-  }
+//     if ($target.hasClass("fa-solid")) {
+//       $target.closest("i").toggleClass("fa-solid fa-regular");
+//       await currentUser.deleteFavorite(story);
+//       putMyFavoritesStoriesOnPage();
+//     } else {
+//       $target.closest("i").toggleClass("fa-solid fa-regular");
+//       await currentUser.addFavorite(story);
+//     }
+//   }
   
-  $allStoryLists.on("click", ".fa-heart", toggleFavoriteStatus);
+//   $allStoryLists.on("click", ".fa-heart", toggleFavoriteStatus);
